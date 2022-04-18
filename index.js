883dca97464dc2001775d2f091f8ac66119dde54
Nikki Jaramillo <jaramillo.nikki@gmail.com>
2022-04-18 05:06:30 -0700

    modified styles of elements

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+    dogImages[i].style.transform = 'scaleY(-1)'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i < dogNames.length; i++){
+    dogNames[i].style.textAlign = "left"
+}
+
+let footerText = document.querySelector('.footer')
+footerText.style.color = 'darkgreen'
